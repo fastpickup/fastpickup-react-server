11452bdfdde03abf66eaa0d27c5414cca92580ac
kwonjonny <128343377+kwonjonny@users.noreply.github.com>
2023-08-07 09:46:32 +0900

    my page develope commit

--- src/api/memberAPI.js
+++ src/api/memberAPI.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-export const postLogin = async(params) => {
+// Login Form Data UserName , Password Post 
+export const postLogin = async (params) => {
   const formData = new URLSearchParams();
   formData.append('username', params.username);
   formData.append('password', params.password);
@@ -10,4 +11,30 @@ export const postLogin = async(params) => {
   });
 
   return res.data
+}
+
+// Post Api Member Update 
+export const postMemberUpdate = async (params) => {
+  const res = await axios.get(`http://localhost:8081/api/member/update`, params)
+  return res.data;
+}
+
+// Get API Member Update 
+export const getMemberUpdate = async (email) => {
+  const res = await axios.get(`http://localhost:8081/api/member/update/${email}`)
+  return res.data;
+}
+
+// Get Api Member Read 
+export const getMemberREad = async (email) => {
+  const res = await axios.get(`http://localhost:8081/api/member/read/${email}`)
+  return res.data;
+}
+
+
+export const refreshAccessToken = async (refreshToken) => {
+  const res = await axios.get('http://localhost:8081/api/member/refresh', {
+    headers: { 'Authorization': `Bearer ${refreshToken}` }
+  });
+  return res.data;
 }
---
+++ src/hooks/useCustomLogin.js
@@ -0,0 +1,33 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const useCustomLogin = (fn) => {
+
+    // selector가 필요하다 
+    // login 이라는 state를 뽑아온다 
+    const loginInfo = useSelector(state => state.login)
+    
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // 받은 함수가 있으면 실행한다 
+        if (fn) {
+            if (!loginInfo.email) {
+                fn(navigate)
+            }
+            return
+        }
+
+        console.log("signed:" + loginInfo.email)
+        if (!loginInfo.email) {
+            navigate("/member/login")
+        }
+    }, [loginInfo.email])
+
+
+    return { loginInfo }
+
+}
+
+export default useCustomLogin;
