bd75ae654a1f08bbc1d8eb12c3b0be71045dc12a
JY LEE <wndyd0110@gmail.com>
2023-08-11 12:58:04 +0900

    storeReviewPaging

--- src/api/reviewAPI.js
+++ src/api/reviewAPI.js
@@ -16,13 +16,13 @@ export const getReviewList = async (email ,queryObj) => {
 }
 
 // Store Review List
-export const getReviewByStore = async (sno) => {
+export const getReviewByStore = async (sno, page = 2) => {
 
     // queryObj를 URL 쿼리 문자열로 변환한 후 문자열로 저장
     // const queryString = createSearchParams(queryObj)
 
     // QueryString을 axios.get 메소드 요청 URL 파라미터로 사용
-    const res = await jwtAxios.get(`http://localhost:8081/api/review/store/${sno}`)
+    const res = await jwtAxios.get(`http://localhost:8081/api/review/store/${sno}?page=${page}`)
 
     return res.data
 
